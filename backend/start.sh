dc11580d4ff61148168ab0a03bd29bbb3bf1b8ac
Maurice Dramou <mauriceleontine6@gmail.com>
2026-07-31 22:48:16 +0000

    Use python3/pip3 in Replit startup script

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-python -m pip install -r requirements.txt
-python -m uvicorn app.main:app --host=0.0.0.0 --port "${PORT:-8000}"
+pip3 install -r requirements.txt
+python3 -m uvicorn app.main:app --host=0.0.0.0 --port "${PORT:-8000}"
